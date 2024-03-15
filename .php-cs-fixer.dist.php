a1b6d25b590d69223b4db8a5089110a2e9be8955
Nick Been <66799549+nickbeen@users.noreply.github.com>
2024-03-15 13:04:01 +0100

    Fix PHP CS fixer GitHub Action (#1)
    * Update .php-cs-fixer.dist.php
    * Update php-cs-fixer.yml

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -146,15 +146,10 @@ $rules = [
 
 $finder = Finder::create()
     ->in([
-        __DIR__ . '/app',
-        __DIR__ . '/config',
-        __DIR__ . '/database',
-        __DIR__ . '/resources',
-        __DIR__ . '/routes',
+        __DIR__ . '/src',
         __DIR__ . '/tests',
     ])
     ->name('*.php')
-    ->notName('*.blade.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
